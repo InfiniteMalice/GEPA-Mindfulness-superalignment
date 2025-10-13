180d6a467a0166bda92d13180d1807ae7bb5bf52
InfiniteMalice <115899879+InfiniteMalice@users.noreply.github.com>
2025-10-13 09:35:25 -0400

    Update run_cpu_demo.py

--- examples/run_cpu_demo.py
+++ examples/run_cpu_demo.py
@@ -1,9 +1,1 @@
-"""Convenience wrapper for running the CPU demo from the examples directory."""
 
-import sys
-
-from gepa_mindfulness.examples.cpu_demo.run_cpu_demo import main
-
-
-if __name__ == "__main__":
-    main()
