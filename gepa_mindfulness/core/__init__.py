1b1fe1ca99812292753242b198af81d8b81c21dd
InfiniteMalice <115899879+InfiniteMalice@users.noreply.github.com>
2025-12-13 14:17:28 -0500

    Adjust abstention case 11 reward

--- gepa_mindfulness/core/__init__.py
+++ gepa_mindfulness/core/__init__.py
@@ -1,31 +1,49 @@
 """Core GEPA logic exports."""
 
 from .abstention import ABSTAIN_OUTPUT, ConfidenceDecision, enforce_abstention
+from .abstention_rewards import (
+    AbstentionReward,
+    AbstentionRewardWeights,
+    compute_abstention_reward,
+    is_abstention_response,
+)
 from .adversarial import AdversarialScenario, iterate_adversarial_pool, sample_adversarial_batch
 from .contemplative_principles import ContemplativePrinciple, GEPAPrinciples, GEPAPrincipleScore
 from .imperatives import AlignmentImperative, ImperativeEvaluator, ImperativeSignal
 from .paraconsistent import ParaconsistentTruthValue, dialetheic_and
 from .rewards import RewardSignal, RewardWeights
+from .thought_alignment import (
+    classify_thought_alignment,
+    compute_epistemic_score,
+    compute_match_score,
+)
 from .tracing import CircuitTracerLogger, ThoughtTrace, TraceEvent
 
 __all__ = [
     "ABSTAIN_OUTPUT",
-    "ConfidenceDecision",
-    "enforce_abstention",
+    "AbstentionReward",
+    "AbstentionRewardWeights",
     "AdversarialScenario",
-    "iterate_adversarial_pool",
-    "sample_adversarial_batch",
+    "AlignmentImperative",
+    "CircuitTracerLogger",
+    "ConfidenceDecision",
     "ContemplativePrinciple",
     "GEPAPrincipleScore",
     "GEPAPrinciples",
-    "AlignmentImperative",
     "ImperativeEvaluator",
     "ImperativeSignal",
     "ParaconsistentTruthValue",
-    "dialetheic_and",
     "RewardSignal",
     "RewardWeights",
-    "CircuitTracerLogger",
     "ThoughtTrace",
     "TraceEvent",
+    "classify_thought_alignment",
+    "compute_abstention_reward",
+    "compute_epistemic_score",
+    "compute_match_score",
+    "dialetheic_and",
+    "enforce_abstention",
+    "is_abstention_response",
+    "iterate_adversarial_pool",
+    "sample_adversarial_batch",
 ]
